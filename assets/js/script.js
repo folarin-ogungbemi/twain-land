1f67a8e5a5e416ca7580be10594865003bec6777
Folarin Ogungbemi <Folarine1@gmail.com>
2022-09-14 21:57:37 +0000

    Add hero modal code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-document.querySelector('.hero-main').innerText = "Welcome to Twain-Land";
+// Modal opens a dialog box at the click of hero explore button
+
+ document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('.btn--open').onclick = function(){
+        document.querySelector('#hero__modal').showModal();
+    };    
+})
