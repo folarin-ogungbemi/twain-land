2aa430cac21fe48a7b883faa1d9dda49807f6303
Folarin Ogungbemi <Folarine1@gmail.com>
2022-09-15 12:02:45 +0000

    Add toggler function code to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,21 @@
     document.querySelector('.btn--open').onclick = function(){
         document.querySelector('#hero__modal').showModal();
     };    
-})
+});
+
+// Wait for the DOM to finish loading before the game starts
+ // Get the class array "twain-game__card" and listen for a click on each item.
+
+ document.addEventListener('DOMContentLoaded', function(){
+    const countries = document.querySelectorAll('.twain-game__card');
+    countries.forEach(function(country){
+      country.onclick = toggler;
+    });
+});
+
+/**
+ * operates the toggling of cards when event starts 
+ */
+function toggler(){
+    this.classList.toggle('on-toggle');
+}
