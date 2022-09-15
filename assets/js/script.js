4fdea545198c0617ab72acbdaf427a118631b621
Folarin Ogungbemi <Folarine1@gmail.com>
2022-09-15 21:02:37 +0000

    Add game timer code

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,32 @@
     });
 });
 
+// Global variables necessary for the game
+// Game time variable
+ let twainSeconds;
+ let twainTime;
+ let clearTimeRecord = setInterval(startTimer,1000);
+ let twainMinutes = 240 ; // time in seconds (3 * 60sec)
+
 /**
  * Sets game countdown timer
  */
  function startTimer(){
-    
+    let playerTime = document.querySelector('#game-timer');
+
+  if ((twainTime % 60 === 0) && (twainSeconds % 60 === 0)){
+      clearInterval(clearTimeRecord);
+   }
+   else{
+      // code idea from florin pop youtube
+      twainTime = Math.floor(twainMinutes/60);
+      twainSeconds = twainMinutes % 60;
+      twainMinutes--;
+      if(twainSeconds < 10){
+         twainSeconds = '0' + twainSeconds;
+      }
+      }
+      playerTime.innerHTML = `${twainTime}:${twainSeconds}`;
 }
 
 /**
