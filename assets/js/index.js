0d3a096a8e92dcc686f3f719a8888a27f4ed67a1
Folarin Ogungbemi <Folarine1@gmail.com>
2022-09-16 09:00:43 +0000

    Add comment to script

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
-// Modal opens a dialog box at the click of hero explore button
+// Wait for the DOM to finish loading
+// open a dialog box at the click of hero explore button
 
-document.addEventListener('DOMContentLoaded',function(){
-    document.querySelector('.btn--open').onclick = function(){
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.btn--open').onclick = function () {
         document.querySelector('#hero__modal').showModal();
-      };    
+    };
 });
